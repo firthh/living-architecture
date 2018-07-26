a5ba1a45a8a2d6aa575be17b337a532e955ff09c
James Turley <jamesturley@gocardless.com>
2018-07-26 10:08:43 +0100

    Wrap listeners in fn

--- src/cljs/living_architecture/core.cljs
+++ src/cljs/living_architecture/core.cljs
@@ -3,7 +3,7 @@
    [reagent.core :as reagent]
    [re-frame.core :as re-frame]
    [living-architecture.events :as events]
-   [living-architecture.event-listeners] ; not used but the code needs to be required
+   [living-architecture.event-listeners :refer [init-listeners!]] ; not used but the code needs to be required
    [living-architecture.views :as views]
    [living-architecture.config :as config]
    ))
@@ -20,6 +20,7 @@
                   (.getElementById js/document "app")))
 
 (defn ^:export init []
+  (init-listeners!)
   (re-frame/dispatch-sync [::events/initialize-db])
   (dev-setup)
   (mount-root))
--- src/cljs/living_architecture/event_listeners.cljs
+++ src/cljs/living_architecture/event_listeners.cljs
@@ -6,20 +6,21 @@
             [day8.re-frame.tracing :refer-macros [fn-traced defn-traced]]
             ))
 
-(re-frame/reg-event-db
- ::events/initialize-db
- (fn-traced [_ _]
-            db/default-db))
+(defn init-listeners! []
+  (re-frame/reg-event-db
+    ::events/initialize-db
+    (fn-traced [_ _]
+               db/default-db))
 
-;; Can't use namespaced keywords here as
-;; we get cyclic dependencies between this and the http namespace
-(re-frame/reg-event-db
- ::events/new-diagram
- (fn-traced [db [_ response]]
-            (merge db response)))
+  ;; Can't use namespaced keywords here as
+  ;; we get cyclic dependencies between this and the http namespace
+  (re-frame/reg-event-db
+    ::events/new-diagram
+    (fn-traced [db [_ response]]
+               (merge db response)))
 
-(re-frame/reg-event-db
- ::events/get-diagram
- (fn-traced [db [_ diagram-id]]
-            (http/get-diagram diagram-id)
-            db))
+  (re-frame/reg-event-db
+    ::events/get-diagram
+    (fn-traced [db [_ diagram-id]]
+               (http/get-diagram diagram-id)
+               db)))
